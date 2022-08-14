d60ed70cac8e3f48634f73867913f92d1ee80760
hylu-dev <roylu99@gmail.com>
2022-08-14 19:07:36 -0400

    Add ChakraUI

---
+++ root/bujots-front/src/bujotsTheme.ts
@@ -0,0 +1,10 @@
+import {extendTheme} from '@chakra-ui/react';
+
+const bujotsTheme = extendTheme({
+    colors: {
+        coverlight: '#b2906a',
+        coverdark: '#332e34'
+    }
+})
+
+export default bujotsTheme;
--- root/bujots-front/src/index.tsx
+++ root/bujots-front/src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+import bujotsTheme from './bujotsTheme'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={bujotsTheme}>
+      <CSSReset/>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
