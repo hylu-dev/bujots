d63662933f1bbb52356ec9bbab2da4494aa3854d
hylu-dev <roylu99@gmail.com>
2022-09-05 04:03:11 -0400

    Place links across all pages

--- root/bujots-front/src/components/journal/JournalLayout.tsx
+++ root/bujots-front/src/components/journal/JournalLayout.tsx
@@ -2,6 +2,9 @@ import { ReactElement, ReactNode } from "react";
 import { motion } from 'framer-motion'
 import { useDispatch } from "react-redux";
 import { setMousePos } from "../../slices/userSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 type Props = {
   children: ReactNode | ReactElement;
@@ -24,6 +27,23 @@ export default function JournalLayout({ children }: Props) {
       3xl:grid-cols-journal-2xl 3xl:grid-rows-journal-2xl
       ">
         {children}
+
+        {/* Footer */}
+        <motion.div className='col-start-2 col-end-3 row-start-3 row-end-4'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}>
+          <div className='w-full flex items-center p-1 gap-2 justify-end'>
+            <motion.a className='text-paper-dark w-fit h-fit' initial={{ opacity: .5 }} whileHover={{ scale: 1.1, opacity: 1 }}
+              target="_blank" href="https://github.com/hylu-dev/bujots">
+              <FontAwesomeIcon icon={faGithub} />
+            </motion.a>
+            <motion.a className='text-paper-dark w-fit h-fit flex justify-center text-xs' initial={{ opacity: .5 }} whileHover={{ scale: 1.1, opacity: 1 }}
+              target="_blank" href="https://hylu.dev">
+              <small><FontAwesomeIcon icon={faHeart} /> Hylu</small>
+            </motion.a>
+          </div>
+        </motion.div>
       </div>
     </motion.div>
   )
--- root/bujots-front/src/pages/Journal.tsx
+++ root/bujots-front/src/pages/Journal.tsx
@@ -73,21 +73,6 @@ export default function Journal() {
             <motion.div className='col-start-3 col-end-4 row-start-2 row-end-3 justify-self-start'>
                 <Timeline></Timeline>
             </motion.div>
-
-            {/* Footer */}
-            <motion.div className='col-start-2 col-end-3 row-start-3 row-end-4'>
-                <div className='w-full flex items-center p-1 gap-2 justify-between'>
-                    <motion.a className='text-paper-dark w-fit h-fit' initial={{ opacity: .5 }} whileHover={{ scale: 1.1, opacity: 1 }}
-                        target="_blank" href="https://github.com/hylu-dev/bujots">
-                        <FontAwesomeIcon icon={faGithub} />
-                    </motion.a>
-                    <motion.a className='text-paper-dark w-fit h-fit flex justify-center text-xs' initial={{ opacity: .5 }} whileHover={{ scale: 1.1, opacity: 1 }}
-                        target="_blank" href="https://hylu.dev">
-                        <small><FontAwesomeIcon icon={faHeart} /> Hylu</small>
-                    </motion.a>
-                </div>
-
-            </motion.div>
         </JournalLayout>
     </>
 
