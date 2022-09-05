bd264fa30554e5e41e1db51a693a0286a13669e0
hylu-dev <roylu99@gmail.com>
2022-09-04 21:22:48 -0400

    Adjust jot styling

--- root/bujots-front/src/components/journal/Jot.tsx
+++ root/bujots-front/src/components/journal/Jot.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { useSelector, useDispatch, } from 'react-redux';
 import { getCurrentPage, setJots } from '../../slices/journalSlice'
 
@@ -12,6 +12,7 @@ type Props = {
 export default function Jot({ text, index, saveHandler }: Props) {
   const page = useSelector(getCurrentPage);
   const dispatch = useDispatch();
+  const [isFocused, setIsFocused] = useState(false);
 
   const removeJot = () => {
     const newJots = [...page.jots];
@@ -30,23 +31,37 @@ export default function Jot({ text, index, saveHandler }: Props) {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}>
-      <input type="text" defaultValue={text}
+      onHoverStart={() => setIsFocused(true)}
+      onHoverEnd={() => setIsFocused(false)}
+    >
+      <motion.input type="text" defaultValue={text}
+        transition={{ ease: [0.075, 0.82, 0.165, 1], duration: .5 }}
+        whileHover={{
+          translateY: -2,
+          background: 'white',
+          boxShadow: '0px 0px 2px #666'
+        }}
         onChange={(e: ChangeEvent<HTMLInputElement>) => updateJot(e.target.value)}
         onBlur={saveHandler}
         className={`w-full
-        focus:bg-white
         outline-none
-        bg-white
-        focus:outline-paper-dark`}
+        outline-1
+        focus:outline-paper-dark
+        bg-transparent`}
       />
-      <motion.button className='grid place-content-center select-none text-white text-[.5rem] rounded-full h-3 w-3 bg-paper-dark
-      absolute -top-1.5 -right-1.5'
-        onClick={removeJot}
-        whileHover={{ scale: 1.3 }}
-        whileTap={{ scale: 1 }}>
-        &#10005;
-      </motion.button>
+      {
+        isFocused && <motion.button className='grid place-content-center select-none text-white text-[.5rem] rounded-full h-3 w-3 bg-paper-dark
+        absolute -top-1.5 -right-1.5'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: [0.075, 0.82, 0.165, 1], duration: .5 }}
+          onClick={removeJot}
+          whileHover={{ scale: 1.3 }}
+          whileTap={{ scale: 1 }}>
+          &#10005;
+        </motion.button>
+      }
+
     </motion.li >
   )
 }
